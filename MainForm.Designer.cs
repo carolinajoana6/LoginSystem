03759facf2cc817037aa22e3b3db88210cb19a32
65caritina65 <65caritina65@gmail.com>
2026-06-17 17:00:17 -0400

    Main Form

---
+++ MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LoginSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblWelcome = new Label();
+            btnAdminPanel = new Button();
+            btnLogout = new Button();
+            SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            lblWelcome.AutoSize = true;
+            lblWelcome.Location = new Point(31, 29);
+            lblWelcome.Name = "lblWelcome";
+            lblWelcome.Size = new Size(57, 15);
+            lblWelcome.TabIndex = 0;
+            lblWelcome.Text = "Welcome";
+            lblWelcome.Click += lblWelcome_Click;
+            // 
+            // btnAdminPanel
+            // 
+            btnAdminPanel.Location = new Point(22, 109);
+            btnAdminPanel.Name = "btnAdminPanel";
+            btnAdminPanel.Size = new Size(75, 23);
+            btnAdminPanel.TabIndex = 1;
+            btnAdminPanel.Text = "Panel";
+            btnAdminPanel.UseVisualStyleBackColor = true;
+            btnAdminPanel.Click += btnAdminPanel_Click;
+            // 
+            // btnLogout
+            // 
+            btnLogout.Location = new Point(22, 197);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(75, 23);
+            btnLogout.TabIndex = 2;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
+            btnLogout.Click += btnLogout_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(304, 293);
+            Controls.Add(btnLogout);
+            Controls.Add(btnAdminPanel);
+            Controls.Add(lblWelcome);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblWelcome;
+        private Button btnAdminPanel;
+        private Button btnLogout;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdminPanel_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Área administrativa (você pode listar usuárioss, " +
+   "etc.)");
+            //Abrir um form de administração, etc.
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.LoggedUser = null;
+            this.Close();
+            Application.Restart(); // Ou reabrir o loginForm
+
+        }
+
+        private void lblWelcome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lblWelcome.Text = $"Bem-Vindo, {Session.LoggedUser.Username}!";
+            if (AuthService.IsInRole(Session.LoggedUser, "Admin"))
+            {
+                btnAdminPanel.Visible = true;
+            }
+            else
+            {
+                btnAdminPanel.Visible = false;
+            }
+        }
+    }
+}
